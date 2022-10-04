9cd98b3867ae0d7652d7e966dd31c419e61b44b8
SaulRC1 <saul.rodriguez.naranjo@gmail.com>
2022-10-04 16:59:48 +0200

    Añadimos codigo a la clase HolaMundo.java

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -4,6 +4,9 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo!");
+		
+		String name = "Saul";
+		System.out.println("Bienvenido, " + name);
 	}
 
 }
