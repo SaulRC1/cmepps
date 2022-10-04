bf1f64c32be4e112eac5ed42a9019a8c2432e48f
usuario <usuario@pc21-109>
2022-10-04 17:26:08 +0200

    Ligera modificacion

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -6,7 +6,7 @@ public class HolaMundo {
 		System.out.println("Hola Mundo!");
 		
 		String name = "Saul";
-		System.out.println("Bienvenido, " + name);
+		System.out.println("Bienveni2, " + name);
 	}
 
 }
